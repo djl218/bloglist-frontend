2cb65e4192423644211f197697a1c2e72e41bfd8
Daniel Leskosky <dleskosky@gmail.com>
2020-07-31 19:21:00 -0400

    Commit for exercise 5.1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React, { useState, useEffect } from 'react'
 import Blog from './components/Blog'
+import LoggedInMessage from './components/LoggedInMessage'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -11,9 +16,65 @@ const App = () => {
     )  
   }, [])
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBloglistappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+      
+    const user = await loginService.login({
+      username, password,
+    })
+
+    window.localStorage.setItem(
+      'loggedBloglistappUser', JSON.stringify(user)
+    )
+
+    blogService.setToken(user.token)
+    setUser(user)
+    setUsername('')
+    setPassword('')
+  }
+
+  if (user === null) {
+    return (
+      <div>
+        <h2>Log in to application</h2>
+        <form onSubmit={handleLogin}>
+          <div>
+          username
+            <input
+              type="text"
+              value={username}
+              name="Username"
+              onChange={({ target }) => setUsername(target.value)}
+            />
+          </div>
+          <div>
+          password
+            <input
+              type="password"
+              value={password}
+              name="Password"
+              onChange={({ target }) => setPassword(target.value)}
+            />
+          </div>
+          <button type="submit">login</button>
+        </form>
+      </div>
+    )
+  }
+
   return (
     <div>
       <h2>blogs</h2>
+      <LoggedInMessage user={user.name} />
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
---
+++ src/components/LoggedInMessage.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const LoggedInMessage = ({ user }) => (
+    <p>{user} logged in</p>
+)
+
+export default LoggedInMessage
